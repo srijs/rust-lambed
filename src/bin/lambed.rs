ce6181894810cdaff99f4b9356fe35e92359bad3
Sam Rijs <srijs@airpost.net>
2015-08-30 15:13:17 +1000

    make repl work without readline

--- src/bin/lambed.rs
+++ src/bin/lambed.rs
@@ -3,15 +3,30 @@ extern crate readline;
 extern crate lambed;
 
 #[cfg(feature = "readline")]
-use std::ffi::CString;
-
-#[cfg(feature = "readline")]
-fn main() {
+fn interact<F: Fn(&String)>(f: F) {
+    use std::ffi::CString;
     let prompt = CString::new("> ").unwrap();
     while let Ok(s) = readline::readline(&prompt) {
         let bytes = s.to_bytes();
         let string = String::from_utf8_lossy(bytes).into_owned().to_string();
-        match lambed::parser::parse_string(&string) {
+        f(&string)
+    }
+}
+
+#[cfg(not(feature = "readline"))]
+fn interact<F: Fn(&String)>(f: F) {
+    use std::io;
+    let mut stdin = io::stdin();
+    let mut string = String::new();
+    while let Ok(_) = stdin.read_line(&mut string) {
+        f(&string);
+        string.clear()
+    }
+}
+
+fn main() {
+    interact(|string| {
+        match lambed::parser::parse_string(string) {
             Err(err) => println!("{:?}", err),
             Ok((term, _)) => {
                 println!("{:?}", term);
@@ -19,5 +34,5 @@ fn main() {
                 println!("{:?}", result)
             }
         }
-    }
+    })
 }
